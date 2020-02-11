4f3f8f8bf356b485f403ee94a0046414002bcf6b
mohamedelaraby <mohamedelaraby1995@gmail.com>
2020-02-11 12:42:57 -0800

    Initial Release

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content=" Video Home which you could watch different Videos in different fields and communities
+		Also You could have your own channel ans upload your videos">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--  [ Style files ] -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<!-- [ Website title ] -->
+	<title>Venture - Your Spot to the world</title>
+</head>
+
+<body>
+	<!-- [ Main container ] -->
+	<div id="pageContainer" class="pageContainer">
+		<!-- [ Navigation top bar] -->
+		<div id="masterHeadContainer" class="masterHeadContainer">
+	Welcome to venture
+		</div>
+
+		<!-- [ Navigation side bar] -->
+		<div id="sideNavContainer" class="sideNavContainer">
+
+		</div>
+
+		<!-- [  Main Section Container ] -->
+		<div id="mainSectionContainer" class="mainSectionContainer">
+		
+		</div>
+	</div>
+</body>
+
+</html>
